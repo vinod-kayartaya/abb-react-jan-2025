3ce2030b40db11d782e5a8f472c4a55946c6ee2a
Vinod Kumar Kayartaya <vinod@vinod.co>
2025-01-24 13:13:00 +0530

    implement delete feature

--- workspace/phone-book-redux-app/src/components/ContactCard.js
+++ workspace/phone-book-redux-app/src/components/ContactCard.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { deleteContact } from '../redux/action-creators/phonebook-action-creators';
+
 import '../styles.css';
 
 const ContactCard = ({ contact }) => {
+  // react-redux provides a hook called useDispatch to dispatch actions
+  const dispatch = useDispatch();
+
   return (
     <>
       <h3>
         {contact.firstname} {contact.lastname}
-        <button className='app-trash-btn btn btn-link'>
+        <button
+          onClick={() => dispatch(deleteContact(contact.id))}
+          className='app-trash-btn btn btn-link'
+        >
           <i className='bi bi-trash'></i>
         </button>
       </h3>
