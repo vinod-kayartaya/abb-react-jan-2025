acbbbb399277a79df8d098e22e38f0e83861c07b
Vinod Kumar Kayartaya <vinod@vinod.co>
2025-01-24 15:22:38 +0530

    json server

--- workspace/phone-book-redux-app/src/redux/reducers/phonebookReducer.js
+++ workspace/phone-book-redux-app/src/redux/reducers/phonebookReducer.js
@@ -8,48 +8,7 @@ import {
 } from '../action-types/phonebook-types';
 
 const initialState = {
-  contacts: [
-    {
-      id: 1,
-      firstname: 'John',
-      lastname: 'Doe',
-      email: 'john.doe@example.com',
-      phone: '123-456-7890',
-      city: 'New York',
-    },
-    {
-      id: 2,
-      firstname: 'Jane',
-      lastname: 'Smith',
-      email: 'jane.smith@example.com',
-      phone: '234-567-8901',
-      city: 'Los Angeles',
-    },
-    {
-      id: 3,
-      firstname: 'Michael',
-      lastname: 'Johnson',
-      email: 'michael.johnson@example.com',
-      phone: '345-678-9012',
-      city: 'Chicago',
-    },
-    {
-      id: 4,
-      firstname: 'Emily',
-      lastname: 'Davis',
-      email: 'emily.davis@example.com',
-      phone: '456-789-0123',
-      city: 'Houston',
-    },
-    {
-      id: 5,
-      firstname: 'David',
-      lastname: 'Wilson',
-      email: 'david.wilson@example.com',
-      phone: '567-890-1234',
-      city: 'Phoenix',
-    },
-  ],
+  contacts: [],
   selectedContact: null,
 };
 
---
+++ workspace/phone-book-rest/Readme.md
@@ -0,0 +1,13 @@
+To create a REST endpoint run this command at the command prompt:
+
+```sh
+npx json-server db.json --port=8080
+```
+
+Sample db.json file:
+
+```json
+{
+    "contacts": []
+}
+```
